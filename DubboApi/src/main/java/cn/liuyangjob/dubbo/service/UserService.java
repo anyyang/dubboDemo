66a4d120ff9d7eff6206b4b99cd4ea2c09f1f72c
liuyang <me@liuyangjob.cn>
2019-09-12 11:02:54 +0800

    刘洋
    init

---
+++ DubboApi/src/main/java/cn/liuyangjob/dubbo/service/UserService.java
@@ -0,0 +1,16 @@
+package cn.liuyangjob.dubbo.service;
+
+/**
+ * Created by  liuyang
+ * 2019/9/12    10:53
+ * cn.liuyangjob.dubbo
+ * All Right Reserved by liuyang.
+ **/
+
+public interface UserService {
+    /**
+     * say hello
+     * @param name
+     */
+     String sayHello(String name);
+}
